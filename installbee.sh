6b92bc7650c308b9497c0f1caac7da80cef70d07
Ace lee <38158900+dacelee@users.noreply.github.com>
2021-02-19 21:11:53 +0800

    Update installbee.sh

--- installbee.sh
+++ installbee.sh
@@ -4,7 +4,3 @@ yum install epel-release -y
 yum list jq 
 yum install jq -y
 mkdir /bee-soft
-wget https://github.com/ethersphere/bee-clef/releases/download/v0.4.7/bee-clef_0.4.7_amd64.rpm
-sudo rpm -i bee-clef_0.4.7_amd64.rpm
-wget https://github.com/ethersphere/bee/releases/download/v0.5.0/bee_0.5.0_amd64.rpm
-sudo rpm -i bee_0.5.0_amd64.rpm
